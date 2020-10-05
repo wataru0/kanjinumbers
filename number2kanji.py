4d261e66827da131305465c7a7e996b9f480e7c9
wataru0 <wataru.tv.nanao@gmail.com>
2020-10-05 18:30:52 +0900

    零の処理以外完成

--- number2kanji.py
+++ number2kanji.py
@@ -1,9 +1,61 @@
 import numpy as np
 
+kansuji = {
+    0:"零",
+    1:"壱",
+    2:"弐",
+    3:"参",
+    4:"四",
+    5:"五",
+    6:"六",
+    7:"七",
+    8:"八",
+    9:"九",
+    10:"拾",
+    100:"百",
+    1000:"千",
+    10000:"万",
+    100000000:"億",
+    1000000000000:"兆"
+}
+kugiri = ["","拾","百","千"]
+keta = ["","万","億","兆"]
+
+
+
+
 def number2kanji(num):
-    kanji = "壱"
+    NumList = []
+    kanji = ""
+    # 下位からから4桁ずつ区切る
+    for _ in range(len(num)//4+1):
+        tmp = num[-4:]
+        NumList.append(tmp)
+        # 末尾の文字列を削除
+        num = num.rstrip(tmp)
+
+    # リストを逆順にする
+    NumList = NumList[::-1]
+    # print(NumList)
+    
+    # 4桁ずつ処理していく
+    for index,fourDigit in enumerate(NumList):
+        tmp = ""
+        # 区切りを追加
+        for i in range(len(fourDigit)):
+            tmp += kansuji[int(fourDigit[i])]
+            tmp += kugiri[len(fourDigit)-i-1]
+
+        # 桁を追加
+        digit = len(NumList)-index-1
+        tmp += keta[digit] 
+        kanji += tmp
+
     return kanji
 
+
+# 入力str
 num = input()
 
+# 出力
 print(number2kanji(num))
