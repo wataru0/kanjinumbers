adf8b23046eeabfc94ac4673a5dbfe53812ec91d
wataru0 <wataru.tv.nanao@gmail.com>
2020-10-05 19:35:41 +0900

    kanji2numberの完成

---
+++ kanji2number.py
@@ -0,0 +1,37 @@
+suji = {
+    "零":0,
+    "壱":1,
+    "弐":2,
+    "参":3,
+    "四":4,
+    "五":5,
+    "六":6,
+    "七":7,
+    "八":8,
+    "九":9
+}
+
+keta = {
+    "拾":10,
+    "百":100,
+    "千":1000,
+    "万":10000,
+    "億":100000000,
+    "兆":1000000000000
+}
+def kanji2number(kansuji):
+    number = 0
+    tmp = 0
+    for kanji in kansuji:
+        if kanji in keta:
+            number += tmp*keta[kanji]
+            tmp = 0
+        else:
+            tmp += suji[kanji]
+
+    number += tmp
+    return number
+
+kansuji = input()
+
+print(kanji2number(kansuji))
