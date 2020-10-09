0b5479bea7b20b80a181277577ff0ff01a20dbe8
wataru0 <wataru.tv.nanao@gmail.com>
2020-10-09 20:34:51 +0900

    CORS対策

--- kanjinumbersBack.py
+++ kanjinumbersBack.py
@@ -1,22 +1,31 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, make_response
 from number2kanji import number2kanji as n2k
 from kanji2number import kanji2number as k2n
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 
-# @app.route("/", methods=["GET", "POST"])
-# def main_page():
-#     # render_templateで ./templates/ 内のhtmlファイル読み込める
-#     return render_template("mainpage.html")
+# どこからでもアクセスできるようになる
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET')
+  return response
 
-@app.route("/v1/number2kanji/<number>", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
+def main_page():
+    # render_templateで ./templates/ 内のhtmlファイル読み込める
+    return render_template("index.html")
+
+
+@app.route("/v1/number2kanji/<number>", methods=["GET"])
 def number2kanji(number):
     return jsonify({
         "number": str(number),
         "kanji": str(n2k(number))})
 
-@app.route("/v1/kanji2number/<kanji>", methods=["GET", "POST"])
+@app.route("/v1/kanji2number/<kanji>", methods=["GET"])
 def kanji2number(kanji):
     return jsonify({
         "number": str(k2n(kanji)),
